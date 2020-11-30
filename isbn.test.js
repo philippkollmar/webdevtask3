57779c86db21cf36c5bbc73b576a5aec34fe53c8
Philipp Kollmar <ph-kollmar@web.de>
2020-11-30 21:55:35 +0100

    Added checkISBN funtion and its tests

---
+++ isbn.test.js
@@ -0,0 +1,34 @@
+const {checkISBN} = require('./isbn');
+
+describe('checkISBN', () => {
+
+    it('should return 1', () => {
+        expect(checkISBN("1")).toBe("1");
+    })
+
+    it('should return 3', () => {
+        expect(checkISBN("123")).toBe("3");
+    })
+
+    it('should return 39', () => {
+        expect(checkISBN("396")).toBe("6");
+    })
+
+    it('368008783 should return 7', () => {
+        expect(checkISBN("368008783")).toBe("7");
+    })
+
+    it('3-680-0878 should return 7', () => {
+        expect(checkISBN("3-680-08783")).toBe("7");
+    })
+
+    it('3---------------680-0878 should return 7', () => {
+        expect(checkISBN("3----------------680-08783")).toBe("7");
+    })
+
+    it('3-499-13599 should return X', () => {
+        expect(checkISBN("3-499-13599")).toBe("X");
+    })
+
+    
+})

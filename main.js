3e3c32e31128e821d422329a466d78639619fc03
Philipp Kollmar <ph-kollmar@web.de>
2020-12-04 15:03:19 +0100

    manipulate index if checkISBN is true or false

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const { checkISBN } = require('./isbn')
 
+document.getElementById('isbnOK').style.visibility = 'hidden'
+document.getElementById('isbnCorrupt').style.visibility = 'hidden'
+
 function check() {
     const userinput = document.querySelector('#userinput').value
-    document.querySelector('#result').innerHTML = checkISBN(userinput)
+
+    if (checkISBN(userinput) === true) {
+        document.getElementById('isbnOK').style.visibility = 'visible'
+    }
+    else {
+        document.getElementById('isbnCorrupt').style.visibility = 'visible'
+    }
+    //document.querySelector('#result').innerHTML = checkISBN(userinput)
 }
 
 const button = document.querySelector('#check')
